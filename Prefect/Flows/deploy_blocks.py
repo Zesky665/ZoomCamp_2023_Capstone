917b6ec5b53a38883b94b0a3c9669ea200b4ea05
Zesky665 <zharkoc@gmail.com>
2023-03-07 00:39:37 +0100

    Update deploy_blocks.py

--- Prefect/Flows/deploy_blocks.py
+++ Prefect/Flows/deploy_blocks.py
@@ -43,7 +43,7 @@ def deploy_s3_block():
     contents = f.read()
     # returns JSON object as
     # a dictionary
-    data = json.loads(contents.decode("utf-8"))
+    data = json.load(contents.decode("utf-8"))
     
     logger.info(f'INFO: {data}')
     
