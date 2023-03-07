ad142100a7c3374a427660bd11899a49f4de1ebf
Zesky665 <zharkoc@gmail.com>
2023-03-07 02:43:24 +0100

    Update deploy_blocks.py

--- Prefect/Flows/deploy_blocks.py
+++ Prefect/Flows/deploy_blocks.py
@@ -36,7 +36,7 @@ def deploy_s3_block():
     logger.info(f'INFO: {path1},{path2},{path3},{path4}')
     
     # Opening JSON file
-    f = open('outputs.json')
+    f = open('output.json')
   
     # returns JSON object as 
     # a dictionary
