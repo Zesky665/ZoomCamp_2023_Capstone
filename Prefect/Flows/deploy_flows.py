0ae0a9199e47373de51020c7cc4ad54897c561b8
Zesky665 <zharkoc@gmail.com>
2023-03-09 11:48:20 +0100

    Add flow to script

--- Prefect/Flows/deploy_flows.py
+++ Prefect/Flows/deploy_flows.py
@@ -3,21 +3,25 @@ from test_deploy import test
 from prefect.deployments import Deployment
 from prefect.filesystems import S3
 
-s3_block = S3.load("deployments")
+@flow()
+def deploy_flows():
+    s3_block = S3.load("deployments")
 
-deployment = Deployment.build_from_flow(
-    flow=test,
-    name="test-deploy",
-    parameters={"name": "Test"},
-    infra_overrides={"env": {"PREFECT_LOGGING_LEVEL": "DEBUG"}},
-    work_queue_name="default",
-    storage=s3_block,
-)
+    deployment = Deployment.build_from_flow(
+        flow=test,
+        name="test-deploy",
+        parameters={"name": "Test"},
+        infra_overrides={"env": {"PREFECT_LOGGING_LEVEL": "DEBUG"}},
+        work_queue_name="default",
+        storage=s3_block,
+    )
+    
+    deployment.apply()
 
 if __name__ == "__main__":
     logger = get_run_logger()
     logger.info("INFO: Started flow deployment.")
-    deployment.apply()
+    deploy_flows()
     logger.info("INFO: Finished flow deployment.")
 
 
