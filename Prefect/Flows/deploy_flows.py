d0a1e9bd936960b8e5decdb63efa7d52ff73bfe6
Zesky665 <zharkoc@gmail.com>
2023-03-09 12:23:44 +0100

    Deploy my deploy flow flow

--- Prefect/Flows/deploy_flows.py
+++ Prefect/Flows/deploy_flows.py
@@ -18,11 +18,27 @@ def deploy_test_flow():
     
     deployment.apply()
 
+@task()
+def deploy_deploy_flow():
+    s3_block = S3.load("deployments")
+
+    deployment = Deployment.build_from_flow(
+        flow=deploy_flows,
+        name="deploy-deploy",
+        parameters={"name": "Deploy"},
+        infra_overrides={"env": {"PREFECT_LOGGING_LEVEL": "DEBUG"}},
+        work_queue_name="default",
+        storage=s3_block,
+    )
+    
+    deployment.apply()
+    
 @flow()
 def deploy_flows():
     logger = get_run_logger()
     logger.info("INFO: Started flow deployment.")
     deploy_test_flow()
+    deploy_deploy_flow()
     logger.info("INFO: Finished flow deployment.")
 
 if __name__ == "__main__":
